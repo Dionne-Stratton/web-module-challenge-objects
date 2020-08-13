edf3110e9f177f7f2000cc2947e795e2c5fc1892
Dionne Stratton <mylife33@hotmail.com>
2020-08-13 12:27:25 -0500

    half there

--- index.js
+++ index.js
@@ -4,23 +4,28 @@ const latte = {name: "Cafe Latte", price: 4, category: "Drinks"};
 const burger = {name: "Burger", price: 18, category: "Lunch"};
 const breakfastBurrito = {name: "Breakfast Burrito", price: 16, category:"Breakfast"};
 
+console.log(latte)
 /* Task 1a: write a function to return more menu items with the same format as the items above. */
 
 function createMenuItem(name, cost, category){
-    /* Code here */
+    let newItem = {name, cost, category};
+    return newItem
 }
+console.log(createMenuItem("name", 0, "category"))
 
 /* Task 1b: use your function to create 3 more menu items. You may add any items to the menu that you'd like */
 
+console.log(createMenuItem("Pizza", 10, "Dinner"))
+console.log(createMenuItem("Ice Cream", 5, "Desert"))
+console.log(createMenuItem("Popcorn", 3, "Snack"))
 
+/* Task 2: You're having a lunch special! 25% off for teachers and students, 10% off for everyone else. Add a method to your burger object that automatically calculates price given a string as a parameter.
 
-/* Task 2: You're having a lunch special! 25% off for teachers and students, 10% off for everyone else. Add a method to your burger object that automatically calculates price given a string as a parameter. 
-
-Your method should accept: 
+Your method should accept:
 
 (1) A string (teacher, student, or public)
 
-and should return a number. 
+and should return a number.
 
 For example, burger.discount("teacher") would return 13.5 and burger.discount("public") would return 16.2*/
 
@@ -40,11 +45,28 @@ const reviews = [{name: "Daniela", rating: 5, feedback:"Beautiful atmosphere and
 
 /* Task 3: Console.log just Julius' feedback */
 
+console.log(reviews[5].feedback)
 
 /* Task 4: Add a new rating with your (fictitious) opinions of the restaurant in the same format as the reviews above. */
 
+function addReviews(array, name, rating, feedback){
+  array.push({name, rating, feedback});
+  return array;
+}
+console.log(addReviews(reviews, "Dionne", 1, "bad"))
 
-/* Task 5: Add the following feedback to Reyna's rating - "this place is chill with really cool people, great for getting work done on weekdays"
+
+
+/* Task 5: Add the following feedback to Reyna's rating - "this place is chill with really cool people, great for getting work done on weekdays"  */
+
+
+function changeReview(array, index, region){
+  array[index].region = region;
+  return array;
+}
+
+
+console.log(changeReview(reviews, 7, 'This place is chill with really cool people, great for getting work done on weekdays.'));
 
 /*  Task 6: Write a function to return a review based on the index of the review in the array.
 
@@ -54,37 +76,37 @@ const reviews = [{name: "Daniela", rating: 5, feedback:"Beautiful atmosphere and
 (2) a number which is the desired index in the array.
 
 and should return a string in the format `{name} gave the restaurant a {rating}, and their feedback was: {feedback}`
- * 
+ *
  * For example, if getReviewByIndex is invoked with reviews and the number 0
  * it will return `Daniela gave the restaurant a 5 star review and their feedback was: Beautiful atmosphere and wonderful vegan options!`
 */
 function getReviewByIndex(reviews, index) {
     /* code here */
   }
-  
+
 
 /* Task 7: Write a function to get information about the most recent review called `getLastReview`
 
 getLastReview should accept:
-  (1) an array of objects 
-  
+  (1) an array of objects
+
 and should return a string in the format `name} gave the restaurant a {rating}, and their feedback was: {feedback}`
 
 For example, if getLastReview is invoked passing the reviews array it will return `Reyna gave the restaurant a 3.5 star review and their feedback was: "this place is chill with really cool people, great for getting work done on weekdays"`.
 */
 function getLastReview(/* code here */) {
     /* code here */
-  } 
+  }
 
 
 ///////////////🍔☕️🍽 STRETCH🍔☕️🍽////////////////////
 
-/** STRETCH 1: Write a function called `getReviewByRating` that returns an array containing all reviews in a certain range. Your function should accept: 
+/** STRETCH 1: Write a function called `getReviewByRating` that returns an array containing all reviews in a certain range. Your function should accept:
 
   (1) An array of objects
   (2) A rating
 
-  and should return an array of objects. 
+  and should return an array of objects.
 
   For example, invoking getReviewByRating(reviews, 4) would return [{name: "Miranda", rating: 4, feedback:"fun trivia and cool vibes"},
     {name: "Wen", rating: 4.5, feedback:"I don't leave my house often, but when I do, it's for this place. Highly reccomend."},
@@ -95,14 +117,14 @@ function getLastReview(/* code here */) {
     /* code here */
   }
 
-  
-/** STRETCH 2: Write a function called 'getLongestReview' that returns an array containing all reviews longer than 15 words. 
-  
-Your function should accept: 
+
+/** STRETCH 2: Write a function called 'getLongestReview' that returns an array containing all reviews longer than 15 words.
+
+Your function should accept:
 
   (1) An array of objects
 
-and should return an array of objects. 
+and should return an array of objects.
 
   For example, invoking getLongReviews(reviews) would return [
     {name: "Wen", rating: 4.5, feedback:"I don't leave my house often, but when I do, it's for this place. Highly reccomend."},
@@ -112,15 +134,15 @@ and should return an array of objects.
   function getLongReviews(/* code here */) {
     /* code here */
   }
-  
 
-/* STRETCH 3:  This challenge is not related to the data above! 
 
-Write a function called carMarker 
+/* STRETCH 3:  This challenge is not related to the data above!
+
+Write a function called carMarker
 
 Your function should accept:
 
-(1) a single odometer argument (a number) 
+(1) a single odometer argument (a number)
 
 and return an object.
 
@@ -134,5 +156,5 @@ The returned object should have the following characteristics:
 
 function carMaker(/* code here */) {
     /* code here */
-    
+
 }
